8c6e6e21ec9726c15686fea5a4e7e523d09d917b
Benjamin Groseclose <benjamin.groseclose@hotmail.com>
2018-02-16 22:21:40 -0500

    Made additionally changes and renamed the Timer class to Gamer class. Also worked out the intent between the SettingsPage, and the Game class

--- app/src/main/java/com/bengroseclose/sleuthinspace/SettingsPage.java
+++ app/src/main/java/com/bengroseclose/sleuthinspace/SettingsPage.java
@@ -1,3 +1,13 @@
+/*
+    This Setting Page will be opened before the game begins and ask the user to enter some
+    required information. How many players, the level of difficulty {Easy, Medium, Hard}, then
+    hopefully implement a QR code scanner what may be attached to each of the cards.
+    Will prompt the user to use the QR code scanner or there will be a manual entry option.
+
+    Either way there will be the same start button found at the bottom of the page that will
+    offically begin the game.
+ */
+
 package com.bengroseclose.sleuthinspace;
 
 import android.content.Intent;
@@ -7,11 +17,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class SettingsPage extends AppCompatActivity {
 
+    public static final String EXTRA_MODE = "com.bengroseclose.sleuthinspace.MESSAGE";
+    public static final String EXTRA_NUMBEROFPLAYERS = "com.bengroseclose.sleuthinspace.MESSAGE";
+
+    int numberOfPlayers;
+    String mode;
+    Spinner spinner_number_of_players = (Spinner) findViewById(R.id.spinner_number_of_players);
+    Spinner spinner_mode = (Spinner) findViewById(R.id.spinner_mode);
+    final Button button_startgame = (Button) findViewById(R.id.button_startgame);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +40,33 @@ public class SettingsPage extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
-        Spinner spinner_number_of_players = (Spinner) findViewById(R.id.spinner_number_of_players);
-        ArrayAdapter<CharSequence> adapter_number_of_players = ArrayAdapter.createFromResource(this,
-                R.array.mode_array, R.layout.spinner_item);
-        adapter_number_of_players.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_number_of_players.setAdapter(adapter_number_of_players);
+        initPage();
 
+        button_startgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGame();
+            }
+        });
 
-        Spinner spinner_mode = (Spinner) findViewById(R.id.spinner_mode);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.mode_array, R.layout.spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_mode.setAdapter(adapter);
+    }
+
+    /*
+        This function will be called when the user presses the start button. Will take the given
+        values from the game mode spinner and the number of players spinner. Save those in an
+        intent then start the game activity.
+     */
+    void startGame()
+    {
+
+        numberOfPlayers = (int) spinner_number_of_players.getSelectedItem();
+        mode = spinner_mode.getSelectedItem().toString();
+
+        Intent intent = new Intent(this, Game.class);
+
+        intent.putExtra(EXTRA_MODE, mode);
+        intent.putExtra(EXTRA_NUMBEROFPLAYERS, numberOfPlayers);
+        startActivity(intent);
 
     }
 
@@ -78,5 +113,20 @@ public class SettingsPage extends AppCompatActivity {
     void openHomePage() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    void initPage(){
+
+        ArrayAdapter<CharSequence> adapter_number_of_players = ArrayAdapter.createFromResource(this,
+                R.array.mode_array, R.layout.spinner_item);
+        adapter_number_of_players.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_number_of_players.setAdapter(adapter_number_of_players);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.mode_array, R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_mode.setAdapter(adapter);
+
     }
 }
